8175c0d6f6e6eab74f69a4631e9a81e682043f99
Advertising-Infotech <advertisingpropaganda@gmail.com>
2026-02-28 01:12:44 -0300

    feat: Add Reddit link to profile header
    Introduces a Reddit icon and link to the social media section of the profile header, expanding the contact options.

--- components/ProfileHeader.tsx
+++ components/ProfileHeader.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import { Instagram, Twitter, Facebook, Send } from 'lucide-react';
 
+const RedditIcon = ({ size = 20 }: { size?: number }) => (
+  <svg 
+    width={size} 
+    height={size} 
+    viewBox="0 0 24 24" 
+    fill="currentColor" 
+  >
+    <path d="M16.5 12c0-1.1-.9-2-2-2-.3 0-.6.1-.9.2-.7-.5-1.7-.8-2.8-.9l.6-2.6 1.9.4c.1.5.5.9 1 .9 1.1 0 2-.9 2-2s-.9-2-2-2c-.8 0-1.5.5-1.8 1.2l-2.1-.5c-.2 0-.4.1-.5.3l-.7 3.1c-1.1.1-2.1.4-2.8.9-.3-.1-.6-.2-.9-.2-1.1 0-2 .9-2 2 0 .8.5 1.5 1.2 1.8-.1.2-.1.4-.1.6 0 2.2 2.7 4 6 4s6-1.8 6-4c0-.2 0-.4-.1-.6.7-.3 1.2-1 1.2-1.8zm-10 0c0-.6.4-1 1-1s1 .4 1 1-.4 1-1 1-1-.4-1-1zm6.7 3.1c-.5.5-1.4.8-2.2.8s-1.7-.3-2.2-.8c-.2-.2-.2-.5 0-.7s.5-.2.7 0c.3.3.9.5 1.5.5s1.2-.2 1.5-.5c.2-.2.5-.2.7 0s.2.5 0 .7zm.3-2.1c-.6 0-1-.4-1-1s.4-1 1-1 1 .4 1 1-.4 1-1 1z"/>
+  </svg>
+);
+
 export const ProfileHeader: React.FC = () => {
   return (
     <div className="flex flex-col items-center text-center mb-8">
@@ -37,6 +48,9 @@ export const ProfileHeader: React.FC = () => {
         </a>
         <a href="https://t.me/+5562991599031" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-[#D4AF37] transition-colors duration-300 hover:scale-110 transform">
             <Send size={20} />
+        </a>
+        <a href="https://www.reddit.com/user/Tiranossaurus_Rex/" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-[#D4AF37] transition-colors duration-300 hover:scale-110 transform">
+            <RedditIcon size={20} />
         </a>
       </div>
     </div>
